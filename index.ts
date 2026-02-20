b0f68f1f7cb61accc09411b844824dc1b3ddd490
Maxime Moreillon <m.moreillon@gmail.com>
2026-02-20 12:09:02 +0900

    working prototype

---
+++ index.ts
@@ -0,0 +1,56 @@
+import "dotenv/config";
+import express from "express";
+import httpProxy from "http-proxy";
+import createHttpError from "http-errors";
+import cookieParser from "cookie-parser";
+import pug from "pug";
+
+const app = express();
+app.use(cookieParser());
+
+const proxy = httpProxy.createProxyServer();
+
+const {
+  PORT = 3000,
+  TARGET_BASE_URL = "https://google.com",
+  ANOUNCEMENT = "No anouncement",
+  COOKIE_NAME = "anouncement_aknowledged",
+} = process.env;
+
+app.post("/acknowledge", (req, res) => {
+  const {
+    headers: { referer = "/" },
+  } = req;
+  res.cookie(COOKIE_NAME, "true");
+  res.redirect(referer);
+});
+
+app.get("/{*splat}", (req, res) => {
+  const {
+    cookies: { [COOKIE_NAME]: acknowledged },
+    headers: { "user-agent": userAgent = "" },
+  } = req;
+
+  const isBrowser = /Mozilla|Chrome|Safari|Firefox|Edge/.test(userAgent);
+
+  if (!acknowledged && isBrowser) {
+    const compiledFunction = pug.compileFile("index.pug");
+    const html = compiledFunction({
+      message: ANOUNCEMENT,
+    });
+    return res.send(html);
+  }
+
+  const options = {
+    target: TARGET_BASE_URL,
+    changeOrigin: true,
+  };
+
+  proxy.web(req, res, options, (error: any) => {
+    if (error) throw createHttpError(500, error);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Express listening on port ${PORT}`);
+});
